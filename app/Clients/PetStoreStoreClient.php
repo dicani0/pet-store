d1a6ced191716e94f518a3cb48c861839a91ce2d
Dawid Miklas <miklasdawid@gmail.com>
2024-05-17 08:22:38 +0200

    try catch wrap for http requests

--- app/Clients/PetStoreStoreClient.php
+++ app/Clients/PetStoreStoreClient.php
@@ -5,6 +5,7 @@ namespace App\Clients;
 use App\Clients\Contracts\PetStoreClientContract;
 use App\Http\Responses\PetStoreCollectionResponse;
 use App\Http\Responses\PetStoreResponse;
+use Illuminate\Http\Client\ConnectionException;
 use Illuminate\Support\Facades\Http;
 
 class PetStoreStoreClient implements PetStoreClientContract
@@ -13,65 +14,106 @@ class PetStoreStoreClient implements PetStoreClientContract
 
     public function index(string $status): PetStoreCollectionResponse
     {
-        $response = Http::get(url: "{$this->baseUrl}/pet/findByStatus", query: ['status' => $status]);
+        try {
+            $response = Http::get(url: "{$this->baseUrl}/pet/findByStatus", query: ['status' => $status]);
+        } catch (ConnectionException $e) {
+            return PetStoreCollectionResponse::fromArray(data: [
+                'status_code' => 500,
+                'success' => false,
+                'data' => ['message' => $e->getMessage()],
+            ]);
+        }
 
         return PetStoreCollectionResponse::fromArray(data: array_merge(
             [
                 'status_code' => $response->status(),
                 'success' => $response->successful(),
             ],
-            ['data' => $response->json()]
+            ['data' => $response->json() ?? []]
         ));
     }
 
     public function find(string $id): PetStoreResponse
     {
-        $response = Http::get(url: "{$this->baseUrl}/pet/{$id}");
+        try {
+            $response = Http::get(url: "{$this->baseUrl}/pet/{$id}");
+        } catch (ConnectionException $e) {
+            return PetStoreResponse::fromArray(data: [
+                'status_code' => 500,
+                'success' => false,
+                'pet' => null,
+            ]);
+        }
+
         return PetStoreResponse::fromArray(data: array_merge(
             [
                 'status_code' => $response->status(),
                 'success' => $response->successful(),
             ],
-            $response->json()
+            $response->json() ?? []
         ));
     }
 
     public function store(array $data): PetStoreResponse
     {
-        $response = Http::post(url: "{$this->baseUrl}/pet", data: $data);
+        try {
+            $response = Http::post(url: "{$this->baseUrl}/pet", data: $data);
+        } catch (ConnectionException $e) {
+            return PetStoreResponse::fromArray(data: [
+                'status_code' => 500,
+                'success' => false,
+                'pet' => null,
+            ]);
+        }
 
         return PetStoreResponse::fromArray(data: array_merge(
             [
                 'status_code' => $response->status(),
                 'success' => $response->successful(),
             ],
-            $response->json()
+            $response->json() ?? []
         ));
     }
 
     public function update(array $data): PetStoreResponse
     {
-        $response = Http::put(url: "{$this->baseUrl}/pet", data: $data);
+        try {
+            $response = Http::put(url: "{$this->baseUrl}/pet", data: $data);
+        } catch (ConnectionException $e) {
+            return PetStoreResponse::fromArray(data: [
+                'status_code' => 500,
+                'success' => false,
+                'pet' => null,
+            ]);
+        }
 
         return PetStoreResponse::fromArray(data: array_merge(
             [
                 'status_code' => $response->status(),
                 'success' => $response->successful(),
             ],
-            $response->json()
+            $response->json() ?? []
         ));
     }
 
     public function delete(string $id): PetStoreResponse
     {
-        $response = Http::delete(url: "{$this->baseUrl}/pet/{$id}");
+        try {
+            $response = Http::delete(url: "{$this->baseUrl}/pet/{$id}");
+        } catch (ConnectionException $e) {
+            return PetStoreResponse::fromArray(data: [
+                'status_code' => 500,
+                'success' => false,
+                'pet' => null,
+            ]);
+        }
 
         return PetStoreResponse::fromArray(data: array_merge(
             [
                 'status_code' => $response->status(),
                 'success' => $response->successful(),
             ],
-            $response->json()
+            $response->json() ?? []
         ));
     }
 }
